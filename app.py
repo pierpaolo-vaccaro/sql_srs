c2a52ed85219901adc6c6daa6586a843fe166af7
pierpaolo <pierpaolo@DESKTOP-SM2A7LC.>
2025-03-25 10:06:29 +0100

    added csv files and side bar

--- app.py
+++ app.py
@@ -1,25 +1,63 @@
 import streamlit as st
 import pandas as pd
 import duckdb as db
+import io
 
-data = {"a": [1,2,3], "b": [4,5,6]}
-df = pd.DataFrame(data)
+csv = """
+beverage, price
+orange juice, 2.5
+Expresso, 2
+Tea, 3
+"""
+beverages = pd.read_csv(io.StringIO(csv))
+
+csv2 = """
+food_item, food_price
+cookie, 2.5
+chocolatine, 2
+muffin, 3
+"""
+food_items = pd.read_csv(io.StringIO(csv2))
+
+answer = """
+SELECT *
+FROM beverages
+CROSS JOIN food_items
+"""
+
+solution = db.sql(answer)
+
+
 
 st.write("""
 # SQL SRS
 Spaced Repetition System for SQL practice
 """)
 
-option = st.selectbox(
-    "What would you like to review?",
-    ("Joins", "Group By", "Window Functions"),
-    index=None,
-    placeholder="Select a theme..."
-)
-
-st.write(f"You selected: {option}")
+with st.sidebar:
+    option = st.selectbox(
+        "What would you like to review?",
+        ("Joins", "Group By", "Window Functions"),
+        index=None,
+        placeholder="Select a theme..."
+    )
+    st.write(f"You selected: {option}")
 
 sql_query = st.text_area(label="Insert your SQL query here")
-result = db.query(sql_query)
+if sql_query:
+    result = db.query(sql_query)
+    st.dataframe(result)
 st.write(f"You entered the following input: {sql_query}")
-st.dataframe(result)
+
+tab1, tab2 = st.tabs(["Tables", "Solution"])
+
+with tab1:
+    st.write("table: beverages")
+    st.dataframe(beverages)
+    st.write("table: food_items")
+    st.dataframe(food_items)
+    st.write("Expected output")
+    st.dataframe(solution)
+
+with tab2:
+    st.write(answer)
